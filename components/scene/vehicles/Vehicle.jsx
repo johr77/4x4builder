ce0721e04a35808bbf46bdcb03283881fb69ce55
Tater <johr77@gmail.com>
2026-08-26 05:01:53 -0400

    Scale wheel configurations for improved vehicle dynamics

--- components/scene/vehicles/Vehicle.jsx
+++ components/scene/vehicles/Vehicle.jsx
@@ -49,16 +49,16 @@ const Vehicle = ({ spawnPosition = [0, 0, 0], spawnRotation = [0, 0, 0] }) => {
 	const wheelWidth = (config.rim_width * 2.54) / 100
 
 	// Create wheel configurations
-	const physicsWheels = useMemo(() => {
+		const physicsWheels = useMemo(() => {
 	return wheelPositions.map((wheel, i) => ({
 		ref: wheelRefs[i],
 		axleCs: new Vector3(1, 0, 0),
-		position: new Vector3(...wheel.position),
+		position: new Vector3(...wheel.position).multiplyScalar(SCALE),
 		suspensionDirection: new Vector3(0, -1, 0),
-		maxSuspensionTravel: 0.3,
-		suspensionRestLength: 0.1,
+		maxSuspensionTravel: 0.3 * SCALE,
+		suspensionRestLength: 0.1 * SCALE,
 		suspensionStiffness: 28,
-		radius: (config.tire_diameter * 2.54) / 100 / 2,
+		radius: ((config.tire_diameter * 2.54) / 100 / 2) * SCALE,
 	}))
 }, [wheelPositions, config.tire_diameter])
 
