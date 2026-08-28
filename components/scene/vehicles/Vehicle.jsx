3999e24e5473d82488ab5e5a04ec15ac839b53d4
Tater <johr77@gmail.com>
2026-08-27 23:36:24 -0400

    Adjust collider position and mass for improved vehicle stability

--- components/scene/vehicles/Vehicle.jsx
+++ components/scene/vehicles/Vehicle.jsx
@@ -107,13 +107,13 @@ const Vehicle = ({ spawnPosition = [0, 0, 0], spawnRotation = [0, 0, 0] }) => {
 	// 8/24/26 - .65 seems right (.7 still spins(rarly), .6 hits spots in ground.)
 	// Lower = more stable, more ground scrape. Higher = less scrape, more tippy
 	// 3rd (0),forward / back,Keep 0,—
-	const colliderPosition = useMemo(() => [0,1.2 * SCALE ,-0.05])
+	const colliderPosition = useMemo(() => [0,1.2 * SCALE ,-0.04])
 
 	return (
 		<>
 			<RigidBody ref={chassisRef} type='dynamic' position={spawnPosition}
 							rotation={spawnRotation} colliders={false} canSleep={false} linearDamping={0.05} angularDamping={1}>
-				<CuboidCollider args={colliderArgs} position={colliderPosition} mass={6} />
+				<CuboidCollider args={colliderArgs} position={colliderPosition} mass={7} />
 				<group ref={chassisGroupRef} name='Vehicle' scale={[SCALE, SCALE, SCALE]}>
 					<VehicleAudio />
 					<Suspense fallback={null}>
