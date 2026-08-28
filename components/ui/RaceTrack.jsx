59a2472fa2289299d984de6b98c879ec1aa3caa2
Tater <johr77@gmail.com>
2026-08-28 19:27:44 -0400

    Enable DEBUG mode for Physics component in RaceTrack

--- components/ui/RaceTrack.jsx
+++ components/ui/RaceTrack.jsx
@@ -92,7 +92,7 @@ export default function RaceTrack() {
 				<Suspense fallback={<Loader />}>
 				<CameraManager />
 					<InputManager />
-					<Physics >
+					<Physics DEBUG>
 					<Vehicle 
 					spawnPosition={[
 						-((PARTS_ACROSS * SIZE) / 2) + SIZE / 2 + 1 * SIZE,
