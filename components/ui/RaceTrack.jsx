9cdb6fcb4008033826ced0e16324d110bf41fd99
Tater <johr77@gmail.com>
2026-08-28 19:30:44 -0400

    Fix casing for DEBUG prop in Physics component

--- components/ui/RaceTrack.jsx
+++ components/ui/RaceTrack.jsx
@@ -92,7 +92,7 @@ export default function RaceTrack() {
 				<Suspense fallback={<Loader />}>
 				<CameraManager />
 					<InputManager />
-					<Physics DEBUG>
+					<Physics debug>
 					<Vehicle 
 					spawnPosition={[
 						-((PARTS_ACROSS * SIZE) / 2) + SIZE / 2 + 1 * SIZE,
