3411daf6979d215df1fecf132320247b3fdb6a0f
Tater <johr77@gmail.com>
2026-08-27 21:54:32 -0400

    Refactor collider arguments for improved vehicle dynamics

--- components/scene/vehicles/Vehicle.jsx
+++ components/scene/vehicles/Vehicle.jsx
@@ -100,7 +100,7 @@ const Vehicle = ({ spawnPosition = [0, 0, 0], spawnRotation = [0, 0, 0] }) => {
 	// 8/24/26 lifts car on acceleration(less), lifts car on braking, .4 seem right (higher slower)
 	// 3rd (wheelbase / 2 + axleHeight),half length,Front–back,
 	// Bigger = nose/tail hit jumps and ramps sooner
-	const colliderArgs = useMemo(() => [.4, (.5*[scale]), wheelbase / 2 + axleHeight], [wheelbase, axleHeight])
+	const colliderArgs = useMemo(() => [.4,.5, wheelbase / 2 + axleHeight], [wheelbase, axleHeight])
 	// colliderPosition
 	// 1st (0),left / right,Keep 0,—
 	// 2nd (1),up / down,Center of the box. 
