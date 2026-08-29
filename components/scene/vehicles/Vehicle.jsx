3179334f1b4eea84e102711d0a1687b57d784fff
Tater <johr77@gmail.com>
2026-08-29 11:06:44 -0400

    Update suspension stiffness for improved vehicle handling

--- components/scene/vehicles/Vehicle.jsx
+++ components/scene/vehicles/Vehicle.jsx
@@ -57,9 +57,10 @@ const Vehicle = ({ spawnPosition = [0, 0, 0], spawnRotation = [0, 0, 0] }) => {
 		suspensionDirection: new Vector3(0, -1, 0),
 		maxSuspensionTravel: 0.3,
 		suspensionRestLength: 0.1,
-		suspensionStiffness: 28,
+		suspensionStiffness: 60,
+		//suspensionStiffness: 28,
 		radius: (config.tire_diameter * 2.65) / 100 / 2,
-		//radius: (config.tire_diameter * 2.54) / 100 / 2,
+		//radius: (config.tire_diameter * 2.54) / 100 / 2, (tires up/down) visual
 	}))
 }, [wheelPositions, config.tire_diameter])
 
