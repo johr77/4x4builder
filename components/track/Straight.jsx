0a66d955156e48600fd5c64dcf1d64c09d37470f
Tater <johr77@gmail.com>
2026-08-29 15:16:35 -0400

    Fix BERM_H constant value in Straight component for correct berm height

--- components/track/Straight.jsx
+++ components/track/Straight.jsx
@@ -7,7 +7,7 @@ import MudRoad from './MudRoad'
 import OuterWall from './OuterWall'
 
 const BERM_W = 4
-const BERM_H = .001
+const BERM_H = .5
 const STEP_N = 1
 const STEP_W = 1
 const STEP_RISE = 2
