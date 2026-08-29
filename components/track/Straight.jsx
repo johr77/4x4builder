f7fc8466a61b01b9a2ba38fb8f039ce54665c740
Tater <johr77@gmail.com>
2026-08-29 15:10:54 -0400

    Fix BERM_W and BERM_H constant values in Straight component for correct dimensions

--- components/track/Straight.jsx
+++ components/track/Straight.jsx
@@ -6,8 +6,8 @@ import { SIZE, WALL } from './TrackConstants'
 import MudRoad from './MudRoad'
 import OuterWall from './OuterWall'
 
-const BERM_W = 3
-const BERM_H = .1
+const BERM_W = 4
+const BERM_H = .001
 const STEP_N = 1
 const STEP_W = 1
 const STEP_RISE = 2
