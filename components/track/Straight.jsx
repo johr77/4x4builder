1aca78203d08af90908b45e9b3f575c38ab55015
Tater <johr77@gmail.com>
2026-08-29 14:54:57 -0400

    Fix BERM_W and BERM_H constant values in Straight component for correct berm dimensions

--- components/track/Straight.jsx
+++ components/track/Straight.jsx
@@ -6,11 +6,11 @@ import { SIZE, WALL } from './TrackConstants'
 import MudRoad from './MudRoad'
 import OuterWall from './OuterWall'
 
-const BERM_W = 2.3
-const BERM_H = .45
+const BERM_W = 3
+const BERM_H = .25
 const STEP_N = 1
 const STEP_W = .25
-const STEP_RISE = 0.1
+const STEP_RISE = 3
 
 function useMudTextures() {
 	const [sand, sandNormal] = useLoader(TextureLoader, [
